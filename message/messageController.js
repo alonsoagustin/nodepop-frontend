439b85ab5d20bf28a19d11c36c148c7fb2bc48d4
Agustin Alonso <alonsoiagustin@gmail.com>
2024-11-19 17:52:12 +0100

    feat: :sparkles: Handle displaying error message in the message container

---
+++ message/messageController.js
@@ -0,0 +1,14 @@
+import { renderMessage } from '../message/messageView.js';
+
+export const messageController = (messageContainer) => {
+  /**
+   *
+   * @param {HTMLElement} messageContainer - El contenedor donde se mostrará el mensaje de error.
+   * @returns {Function} Una funcion "showMessage" que muestra el mensaje (recibido como parametro) en el contenedor especificado.
+   */
+  const showMessage = (message) => {
+    messageContainer.classList.toggle('visually-hidden');
+    messageContainer.innerHTML = renderMessage(message);
+  };
+  return showMessage;
+};
