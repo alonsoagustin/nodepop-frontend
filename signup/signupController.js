a91473d6a23768f73e7b1c33210fd5d1ca0e4153
Agustin Alonso <alonsoiagustin@gmail.com>
2024-11-21 13:18:18 +0100

    feat: :sparkles: Validate form data and dispatch error message on validation failure

---
+++ signup/signupController.js
@@ -0,0 +1,30 @@
+import { fireEvent } from '../posts/postsController.js';
+
+export const signupController = (form) => {
+  // agregamos un listener al form para que escuche eventos del tipo submit
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // obtenemos los datos del formulario
+    const userEmail = form.querySelector('#email').value;
+    const userPassword = form.querySelector('#password').value;
+    const userPasswordConfirm = form.querySelector('#passwordConfirm').value;
+
+    const errors = [];
+
+    // validamos el formato del email
+    if (!validateEmailFormat(userEmail)) {
+      errors.push('Formato de email invalido.');
+    }
+    // validamos la password
+    if (userPassword !== userPasswordConfirm) {
+      errors.push('Las passwords no coinciden.');
+    }
+
+    errors.forEach((error) => {
+      fireEvent(error, 'danger', form);
+    });
+  });
+
+  // llamar a la API para crear el usuario
+};
