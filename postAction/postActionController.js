90711888e61da29aba35d9a5567ea26151ee256f
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 17:44:26 +0100

    feat: :sparkles: add functionality to toggle favorite post status

--- postAction/postActionController.js
+++ postAction/postActionController.js
@@ -69,4 +69,56 @@ export const postActionController = (container) => {
       buttons.forEach((button) => actionButtonContainer.appendChild(button));
     }
   };
+
+  const handleFavoriteButton = (post) => {
+    const favoriteButton = post.querySelector('.favorite__button');
+
+    favoriteButton.addEventListener('click', async () => {
+      const { currentPost, userId, postId } = getPostAndUserData(post);
+      const { favoritedBy } = currentPost;
+
+      // verificamos si el usuario guardó como favorito el anuncio.
+      const isUserFavoritePost = favoritedBy.includes(userId);
+
+      if (favoriteButton.classList.contains('unliked')) {
+        favoriteButton.classList.toggle('unliked');
+        favoriteButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+            </svg>`;
+      } else {
+        favoriteButton.classList.toggle('unliked');
+        favoriteButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+            </svg>
+            `;
+      }
+
+      try {
+        fireNotification('loading', container);
+        if (isUserFavoritePost) {
+          const updateFavoritedBy = favoritedBy.filter((id) => id !== userId);
+          await updatePost(postId, updateFavoritedBy);
+          fireNotification('removeFavorite', container, 'Anuncio eliminado de "Favoritos"');
+        } else {
+          const updateFavoritedBy = [...favoritedBy, userId];
+          await updatePost(postId, updateFavoritedBy);
+          fireNotification('addFavorite', container, 'Anuncio guardado en "Favoritos"');
+        }
+
+        const response = await getPosts();
+        localStorage.setItem('posts', JSON.stringify(response));
+      } catch (error) {
+        // lanzamos un evento del tipo appNotification para iniciar el spinner.
+        fireNotification('loading', container);
+
+        // lazamos un evento del tipo appNotification para informar un  error de conexión.
+        if (error.message === 'Failed to fetch') {
+          fireNotification('danger', container, '¡Ups! No pudimos conectar con el servidor.');
+        }
+        throw error;
+      }
+    });
+  };
 };
