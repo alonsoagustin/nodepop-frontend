fcd1248a1101bbde610d4ad7430a80e595a00f05
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 16:48:09 +0100

    refactor: :recycle: update buildPost to use DOM manipulation and event listeners

--- posts/postsView.js
+++ posts/postsView.js
@@ -1,34 +1,46 @@
-export const buildPost = (post) => {
-  /**
-   * Genera el maquetado HTML para mostrar un anuncio.
-   *
-   * @function
-   * @param {Object} post - El objeto que contiene la información del anuncio.
-   * @param {string} post.name - El título o nombre del anuncio.
-   * @param {string} post.description - La descripción del anuncio.
-   * @param {number} post.price - El precio del anuncio en euros.
-   * @param {string} post.type - El tipo del anuncio.
-   * @returns {string} Una cadena con el código HTML que representa el anuncio.
-   *
-   */
-  return `
-<!-- POST -->
-      <div data-id="${post.id}" class="post d-flex flex-column justify-content-between" style="width:85%; height:100%; overflow: hidden; border-radius: 0.5rem; box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.1); background-color: #fff; min-width:75%">
-        <div class="post__picture" style=" height: 15rem; position: relative; filter: brightness(0.8);">
-          <img src="./assets/img.jpeg" alt="..." class="post__picture-img" style="object-fit: cover;height: 100%; width: 100%;">
+export const buildPost = (postObject) => {
+  const post = document.createElement('article');
+
+  post.setAttribute('id', `${postObject.id}`);
+
+  post.classList.add('post', 'd-flex', 'flex-column', 'justify-content-between');
+
+  post.style.cssText = `
+    padding: 1rem;
+    border-radius : 0.5rem;
+    box-shadow : 0 1.5rem 4rem rgba(0,0,0,0.1);
+    background-color : #fff;
+    min-width: 180px;
+    max-width: 300px;
+    display: flex;
+    align-item: center;
+    justify-content: center;
+    `;
+
+  post.innerHTML = `
+    <div class="post__picture" id="${postObject.id}" style="height: 15rem; filter: brightness(0.8); cursor: pointer">
+      <img src="${postObject.URLImage}" alt="post-image" class="post__picture-img mb-2" style="object-fit: contain; width: 100%; height: 100%">
+    </div>
+    <div class="d-flex flex-column p-3">
+      <div class="post__header p-1">
+        <h5 class="post__title mb-2" style="overflow-wrap: break-word;font-size: 1.3rem; text-transform: uppercase; font-weight: bold;">${postObject.title}</h5>
+      </div>
+      <div class="post__detail p-1">
+        <p class="post__description mb-2" style="font-size: 1rem; font-style: italic;">${postObject.shortDescription}</p>
+      </div>
+      <div class="post__footer d-flex justify-content-between align-item-center gap-3 p-1">
+        <div class="post__price__container">
+          <p class="post__price mb-0" style="font-size: 1.75rem; font-weight: bold;">&#8364 ${postObject.price ?? '0.00'}</p>
         </div>
-        <div class="p-2 d-flex flex-column justify-content-between">
-          <div class="post__header" style="padding: 0 1rem;">
-            <h5 class="post__title mt-2" style="text-align: center;font-size: 1.5; text-transform: uppercase; font-weight: bold;">${post.name}</h5>
-          </div>
-          <div class="post__detail" style="padding: 0 1rem;">
-            <p class="post__description mt-3" style="font-size: 1.2rem; font-style: italic;">${post.description}</p>
-          </div>
-          <div class="post__footer d-flex justify-content-between align-item-center gap-3" style="padding: 0 1rem;">
-            <p class="post__price mb-0" style="font-size: 1.2rem; font-weight: bold;">&#8364 ${post.price}</p>
-            <a href="#" class="btn btn-success">${post.type}</a>
-          </div>
+        <div class="post__action-buttons d-flex align-item-center gap-2">
+
         </div>
       </div>
-  `;
+    </div>`;
+
+  post.addEventListener('click', async () => {
+    localStorage.setItem('postSelected', postObject.id);
+  });
+
+  return post;
 };
