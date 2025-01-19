f4f7d7c6754ec62ba2e68cebe03bb04427c6ad96
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 16:43:09 +0100

    refactor: :recycle: update headerController to use createButton for dynamic button rendering

--- header/headerController.js
+++ header/headerController.js
@@ -1,14 +1,27 @@
-import { builtHeaderButton } from './headerView.js';
+import { createButton } from '../lib/createButton.js';
 
-export const headerController = () => {
-  const div = document.querySelector('header>div');
-  const jwt = localStorage.getItem('jwt');
-  if (jwt) {
-    div.appendChild(builtHeaderButton('Log out', 'btn-light', 'login.html'));
-  } else {
-    div.appendChild(builtHeaderButton('Log in', 'btn-light', 'login.html'));
-    div.appendChild(
-      builtHeaderButton('Sign up', 'btn-outline-light', 'signup.html'),
-    );
-  }
+export const headerController = (buttonContainer) => {
+  const handleHeaderButton = (isAuthenticated) => {
+    if (isAuthenticated) {
+      const logoutButton = { content: 'Log out', style: 'btn-light', href: 'login.html' };
+      const buttons = createButton(logoutButton);
+      buttons.forEach((button) => buttonContainer.appendChild(button));
+    } else {
+      const loginButton = {
+        content: 'Log in',
+        style: 'btn-light',
+        href: 'login.html',
+        htmlContent: null,
+      };
+      const signupButton = {
+        content: 'Sign up',
+        style: 'btn-outline-light',
+        href: 'signup.html',
+        htmlContent: null,
+      };
+      const buttons = createButton(loginButton, signupButton);
+      buttons.forEach((button) => buttonContainer.appendChild(button));
+    }
+  };
+  return { handleHeaderButton };
 };
