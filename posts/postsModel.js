b292b5ddc9cce304019047c12cc0e85e0c489637
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 16:52:31 +0100

    feat: :sparkles: add createPost function for posting new posts with JWT authentication

--- posts/postsModel.js
+++ posts/postsModel.js
@@ -16,3 +16,25 @@ export const getPosts = async (URL = `${config.HOST}${config.POSTS}?_page=1&_lim
     throw error;
   }
 };
+
+export const createPost = async (postObject) => {
+  try {
+    const jwt = getToken();
+
+    const URL = `${config.HOST}${config.POSTS}`;
+
+    // con la extension live-server he tenido ciertos problemas luego de hacer la request
+    const response = await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(postObject),
+      headers: { 'Content-Type': 'application/json', Authorization: `Baerer ${jwt}` },
+    });
+
+    if (!response.ok) {
+      console.error('error al crear el anuncio');
+      throw new Error(response.statusText || 'Error en la solicitud');
+    }
+  } catch (error) {
+    throw error;
+  }
+};
