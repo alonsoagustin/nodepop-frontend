3428995c531a676265c35b6fda9f2f2a8d82270d
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 16:39:16 +0100

    refactor: :recycle: simplify messageController to use createMessage for notifications

--- message/messageController.js
+++ message/messageController.js
@@ -1,28 +1,14 @@
-import { renderMessage, renderWelcomeMessage } from '../message/messageView.js';
+import { createMessage } from '../message/messageView.js';
 
 export const messageController = (messageContainer) => {
-  /**
-   *
-   * @param {HTMLElement} messageContainer - El contenedor donde se mostrará el mensaje de error.
-   * @returns {Function} Una funcion "showMessage" que muestra el mensaje (recibido como parametro) en el contenedor especificado.
-   */
-
-  const showWelcomeMessage = () => {
-    const jwt = localStorage.getItem('jwt');
-    if (jwt) {
-      const payload = JSON.parse(atob(jwt.split('.')[1]));
-      messageContainer.appendChild(renderWelcomeMessage(payload));
-    }
-  };
-
-  const showMessage = (message, type = 'success') => {
-    messageContainer.appendChild(renderMessage(message, type));
+  const showNotification = (content, type) => {
+    messageContainer.appendChild(createMessage(content, type));
     setTimeout(() => {
       const alerts = document.querySelectorAll('.alert');
       alerts.forEach((alert) => {
         alert.classList.add('visually-hidden');
       });
-    }, 5000);
+    }, 2000);
   };
-  return { showMessage, showWelcomeMessage };
+  return { showNotification };
 };
