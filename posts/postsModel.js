781ddf62c451fa2a0fc8bd159809fd58d9997ed1
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 16:50:48 +0100

    refactor: :recycle: update getData to getPosts with error handling and default URL

--- posts/postsModel.js
+++ posts/postsModel.js
@@ -1,16 +1,18 @@
-export const getData = async (url) => {
-  /**
-   * Realiza una solicitud a una API mediante el uso de fetch.
-   *
-   * @async
-   * @function
-   * @param {string} url - La URL de la API a la que se realizará la solicitud.
-   * @throws {Error} Si la respuesta de la API no es exitosa.
-   * @returns {Promise<Object>} La respuesta de la API en formato JSON si es exitosa.
-   */
-  const response = await fetch(url);
-  if (!response.ok)
-    throw new Error('Hubo un problema al procesar la solicitud');
-  const data = await response.json();
-  return data;
+import { config } from '../config.js';
+import { getToken } from '../lib/authUtils.js';
+
+export const getPosts = async (URL = `${config.HOST}${config.POSTS}?_page=1&_limit=10`) => {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    if (!response.ok)
+      throw new Error(
+        `Error ${response.status} (${response.statusText}): Hubo un problema al procesar la solicitud`,
+      );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
