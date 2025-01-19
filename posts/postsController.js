a0980d94c4401ae678d275fcea487a6d05df5700
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 17:14:02 +0100

    refactor: :recycle: update postsController to use getPosts and showPosts, remove JWT handling

--- posts/postsController.js
+++ posts/postsController.js
@@ -1,86 +1,27 @@
-// importamos las funciones necesarios de los módulos correspondientes
-import { getData } from '../posts/postsModel.js';
 import { buildPost } from '../posts/postsView.js';
+import { fireNotification } from '../lib/customEvent.js';
+import { getUserId } from '../lib/authUtils.js';
+import { getPosts } from '../posts/postsModel.js';
 
-export const fireEvent = (message, type, element) => {
-  // creamos un evento personalizado cuando ocurre un error al cargar los anuncios
-  const customEvent = new CustomEvent('userMessage', {
-    detail: {
-      message,
-      type,
-    },
-  });
-  element.dispatchEvent(customEvent);
-};
+export const postsController = (container) => {
+  const getAllPosts = async () => {
+    try {
+      // obtenemos todos los anuncios.
+      const posts = await getPosts();
 
-export const postsController = async (postsContainer) => {
-  /**
-   *
-   *  Gestiona la obtención y renderizado de los anuncios en el DOM.
-   *
-   * @async
-   * @function
-   * @param {HTMLElement} postsContainer - El contenedor HTML donde se mostrarán los anuncios.
-   * @throws {Error} Lanza un error si no hay anuncios disponibles o si ocurre un problema en la request.
-   * @fires CustomEvent#LoadingPostsError - Emite un evento personalizado en caso de error con los detalles del mismo.
-   */
-  const URL = 'http://127.0.0.1:8000/api/posts';
-  try {
-    const jwt = localStorage.getItem('jwt');
+      // guardamos los anuncios en el localStorage.
+      localStorage.setItem('posts', JSON.stringify(posts));
 
-    if (jwt) {
-      const payload = JSON.parse(atob(jwt.split('.')[1]));
-      const userId = payload.userId;
+      localStorage.removeItem('postSelected'); // ????
 
-      const posts = await getData(URL);
-
-      if (posts.length === 0) {
-        // lanzamos un mensaje de error al usuario si no hay anuncios
-        throw new Error('No hay anuncios que mostrar');
+      // llamamos a la función showPosts.
+      showPosts(posts);
+    } catch (error) {
+      // lazamos un evento del tipo appNotification.
+      if (error.message === 'Failed to fetch') {
+        fireNotification('danger', container, '¡Ups! No pudimos conectar con el servidor.');
       }
-
-      postsContainer.classList.toggle('visually-hidden');
-
-      let showPosts = '';
-
-      posts.forEach((post) => {
-        if (post.owner === userId) {
-          const newPost = buildPost(post);
-          showPosts += newPost;
-        }
-      });
-
-      postsContainer.innerHTML = showPosts;
-    } else {
-      // realizamos una solicitud a la API para obtener los anuncios
-      const posts = await getData(URL);
-      if (posts.length === 0) {
-        // lanzamos un mensaje de error al usuario si no hay anuncios
-        throw new Error('No hay anuncios que mostrar');
-      }
-
-      // si hay anuncios, hacemos visible para el usuario el contenedor de anuncios
-      postsContainer.classList.toggle('visually-hidden');
-
-      // acumulador de HTML para los posts
-      let showPosts = '';
-
-      // recorremos el array de posts
-      posts.forEach((post) => {
-        // creamos el html para un post
-        const newPost = buildPost(post);
-
-        //agregamos el html de un post al acumulador
-        showPosts += newPost;
-      });
-
-      //agregamos al DOM el html de todos los posts
-      postsContainer.innerHTML = showPosts;
+      throw error;
     }
-
-    fireEvent(' Anuncios cargados con éxito.', 'success', postsContainer);
-  } catch (error) {
-    // creamos un evento personalizado cuando ocurre un error al cargar los anuncios
-    fireEvent(error.message, 'danger', postsContainer);
-  }
+  };
 };
