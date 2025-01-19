2003457e816475f616490631e8a4898107923923
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 17:21:08 +0100

    fix: :fire: remove existing login logic

--- login/loginController.js
+++ login/loginController.js
@@ -1,26 +1,4 @@
-import { fireEvent } from '../posts/postsController.js';
 import { authenticateUser } from './loginModel.js';
+import { fireNotification } from '../lib/customEvent.js';
 
-export const loginController = (form) => {
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const username = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
-
-    const userObject = { username, password };
-
-    handlerAuthenticateUser(userObject);
-  });
-};
-
-const handlerAuthenticateUser = async (userObject) => {
-  try {
-    const token = await authenticateUser(userObject);
-    localStorage.setItem('jwt', token);
-    window.location.href = 'index.html';
-  } catch (error) {
-    const form = document.querySelector('form');
-    fireEvent(error.message, 'danger', form);
-  }
-};
+export const loginController = (form) => {};
