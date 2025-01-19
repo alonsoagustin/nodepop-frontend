dc16bcc8d92267e212f359dc3f04bf74fcdff3ec
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-19 17:04:38 +0100

    refactor: :recycle: refactor signupController by extracting form data handling logic

--- signup/signupController.js
+++ signup/signupController.js
@@ -1,56 +1,17 @@
-import { fireEvent } from '../posts/postsController.js';
 import { createUser } from './signupModel.js';
-
-export function validateEmailFormat(input) {
-  let regex =
-    /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
-  return regex.test(input);
-}
+import { fireNotification } from '../lib/customEvent.js';
+import { validateEmailFormat } from '../lib/validateEmail.js';
+import { authenticateUser } from '../login/loginModel.js';
 
 export const signupController = (form) => {
-  // agregamos un listener al form para que escuche eventos del tipo submit
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
+  const getFormData = () => {
+    let userEmail, userPassword, userPasswordConfirm;
 
-    // obtenemos los datos del formulario
-    const userEmail = form.querySelector('#email').value;
-    const userPassword = form.querySelector('#password').value;
-    const userPasswordConfirm = form.querySelector('#passwordConfirm').value;
+    // obtenemos los datos ingresados por el usuario.
+    userEmail = form.querySelector('#email').value;
+    userPassword = form.querySelector('#password').value;
+    userPasswordConfirm = form.querySelector('#passwordConfirm').value;
 
-    const errors = [];
-
-    // validamos el formato del email
-    if (!validateEmailFormat(userEmail)) {
-      errors.push('Formato de email invalido.');
-    }
-    // validamos la password
-    if (userPassword !== userPasswordConfirm) {
-      errors.push('Las passwords no coinciden.');
-    }
-
-    errors.forEach((error) => {
-      fireEvent(error, 'danger', form);
-    });
-
-    const userObject = {
-      username: userEmail,
-      password: userPassword,
-    };
-
-    if (errors.length === 0) {
-      // si no hay errores llamamos a la api mediante un metodo POST
-      handlerCreateUser(userObject);
-    }
-  });
-};
-
-const handlerCreateUser = async (userObject) => {
-  try {
-    // hacemos un POST request a la API para crear el usuario
-    await createUser(userObject);
-    window.location.href = 'http://127.0.0.1:8080/index.html';
-  } catch (error) {
-    const form = document.querySelector('form');
-    fireEvent(error.message, 'danger', form);
-  }
+    return { userEmail, userPassword, userPasswordConfirm };
+  };
 };
