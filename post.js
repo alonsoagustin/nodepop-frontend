d8532088c72de200b05f4dde402d09b33276c0cf
Agustin Alonso <alonsoiagustin@gmail.com>
2025-01-20 00:03:28 +0100

    feat: :sparkles: add event listener for appNotification in mainSection to handle different notification types

--- post.js
+++ post.js
@@ -39,4 +39,53 @@ document.addEventListener('DOMContentLoaded', () => {
       handlePostActionButtons();
     }
   }, 600);
+
+  // escuchamos en mainSection un evento del tipo appNotification.
+  mainSection.addEventListener('appNotification', (event) => {
+    const { content, type } = event.detail;
+    if (type === 'loading') {
+      handleSpinner();
+    }
+
+    // cuando el anuncio es agregado a favoritos.
+    if (type === 'addFavorite') {
+      handleSpinner();
+      showNotification(content, 'success');
+    }
+
+    // cuando el anuncio es eliminado de favoritos.
+    if (type === 'removeFavorite') {
+      handleSpinner();
+      showNotification(content, 'success');
+    }
+
+    // cuando el anuncio es eliminado completamente.
+    if (type === 'deleted') {
+      handleSpinner();
+      showNotification(content, 'success');
+      setTimeout(() => {
+        location.href = 'index.html';
+      }, 800);
+    }
+
+    // cuando el anuncio se renderiza correctamente.
+    if (type === 'success') {
+      handleSpinner();
+      showNotification(content, type);
+    }
+
+    if (type === 'danger') {
+      handleSpinner();
+      showNotification(content, type);
+    }
+
+    // cuando el usuario no ha seleccionado ninún anuncio.
+    if (type === 'select') {
+      handleSpinner();
+      showNotification(content, 'danger');
+      setTimeout(() => {
+        location.href = 'index.html';
+      }, 800);
+    }
+  });
 });
